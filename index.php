5b6666edcd07dee2fb4e018a0a060838b057cf4e
柳沢英俊 <yanagisawa@ultrasevenstar.com>
2021-01-13 21:27:09 +0900

    css/js追加

--- index.php
+++ index.php
@@ -24,7 +24,7 @@ function post_data($title, $article) {
 
 function get_file_data() {
     $file = file_get_contents(CSV_FILE);
-    return explode("\n", $file);
+    return array_reverse(explode("\n", $file));
 }
 
 
@@ -52,42 +52,49 @@ if($_SERVER["REQUEST_METHOD"] === "POST"){
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Laravel News</title>
+    <link rel="stylesheet" href="./css/style.css" />
 </head>
 
 <body>
-    <h1>Laravel News</h1>
-    <h2>さぁ、最新のニュースをシェアしましょう</h2>
-    <?php foreach($errors as $error){
+    <main class="main">
+        <h1 class="title"><a href="./">Laravel News</a></h1>
+        <h2>さぁ、最新のニュースをシェアしましょう</h2>
+
+        <?php foreach($errors as $error){
         echo "<p>$error</p>";
-    }
-    ?>
-    <form action="index.php" method="post">
-        <div>
-            <label for="title">タイトル</label>
-            <input type="text" name="title" id="title">
-        </div>
-        <div>
-            <label for="article">記事</label>
-            <textarea name="article" id="article" cols="30" rows="10"></textarea>
-        </div>
-        <div><button type="submit">投稿</button></div>
-    </form>
+        }?>
 
-    <?php foreach($rows as $row) {
-        $data = explode(',', $row);
-        if(empty($data[0])){
-            continue;
-        }
+        <form action="index.php" method="post" onsubmit="return check()">
+            <div class="form-wrapper">
+                <label for="title" class="label">タイトル</label>
+                <input type="text" name="title" id="title" class="input-text">
+            </div>
+            <div class="form-wrapper">
+                <label for="article" class="label">記事</label>
+                <textarea name="article" id="article" cols="30" rows="10" class="input-text"></textarea>
+            </div>
+            <div><button type="submit" class="btn btn--orange">投稿</button></div>
+        </form>
+
+        <div class="article-wrapper">
+            <?php foreach($rows as $row) {
+                $data = explode(',', $row);
+                if(empty($data[0])){
+                    continue;
+                }
 echo <<< EOF
-        <section>
-            <h3>{$data[1]}</h3>
-            <p>{$data[2]}</p>
-            <a href="./posts.php?id={$data[0]}">記事全文・コメントを見る</a>
-        </section>
+            <section class="article">
+                <h3>{$data[1]}</h3>
+                <p>{$data[2]}</p>
+                <a href="./posts.php?id={$data[0]}" class="article-link">
+                    記事全文・コメントを見る
+                </a>
+            </section>
 EOF;
-    }
-    ?>
-
+            }?>
+        </div>
+    </main>
+    <script type="text/javascript" src="./js/index.js"></script>
 </body>
 
 </html>
--- posts.php
+++ posts.php
@@ -6,6 +6,7 @@ define("CSV_COMMENTS", "./csv/comment.csv");
 function find_article($id) {
     $file = file_get_contents(CSV_ARTICLE);
     $rows = explode("\n", $file);
+    
     foreach($rows as $row) {
         $row = explode(',', $row);
         if(empty($row[0])){
@@ -44,13 +45,17 @@ function find_comments($id) {
 // コメント投稿
 function post_comment($post, $article_id) {
     $rows = explode("\n", file_get_contents(CSV_COMMENTS));
+    
+    // 最後の行は空行なのでその一つ前の要素を取得
     $last_row = $rows[array_key_last($rows) - 1];
+    
     if(!$last_row) {
         $id = 1;
     }else{
         $last_row_id = explode(',', $last_row)[0];  
         $id = $last_row_id + 1;
     }
+    
     $fp = fopen(CSV_COMMENTS, "a");
     fputcsv($fp, [$id, $article_id, $post["comment"]]);
 }
@@ -95,38 +100,40 @@ if($_SERVER["REQUEST_METHOD"] === "POST"){
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./css/style.css" />
     <title>Document</title>
 </head>
 
 <body>
-    <h1>Laravel News</h1>
-    <?php
-echo <<< EOF
-    <section>
-        <h3>{$article[1]}</h3>
-        <p>{$article[2]}</p>
-    </section>
-EOF;
-?>
-    <div>
-        <form action=<?php echo "./posts.php?id={$id}"; ?> method="post">
-            <div><textarea name="comment" id="" cols="30" rows="10"></textarea></div>
-            <button type="submit">コメントを書く</button>
-        </form>
-    </div>
+    <main class="main">
+        <h1 class="title"><a href="./">Laravel News</a></h1>
+        <section class="article">
+            <h3><?php echo $article[1]; ?></h3>
+            <p><?php echo nl2br($article[2]); ?></p>
+        </section>
 
-    <?php foreach($comments as $comment) : ?>
-    <div>
-        <p><?php echo $comment[2]; ?></p>
-        <form method="post" action=<?php echo "./posts.php?id={$id}"; ?>>
-            <input type="hidden" value="delete" name="method" />
-            <input type="hidden" value="<?php echo $comment[0]; ?>" name="id" />
-            <button type="submit">コメントを消す</button>
-        </form>
-    </div>
+        <div class="comment-content">
+            <div class="comment-wrapper">
+                <form action=<?php echo "./posts.php?id={$id}"; ?> method="post">
+                    <div>
+                        <textarea name="comment" id="" cols="30" rows="10" class="comment-textarea"></textarea>
+                    </div>
+                    <button type="submit">コメントを書く</button>
+                </form>
+            </div>
 
-    <?php endforeach; ?>
-
+            <?php foreach($comments as $comment) : ?>
+            <div class="comment-wrapper">
+                <p class="comment"><?php echo $comment[2]; ?></p>
+                <form method="post" action=<?php echo "./posts.php?id={$id}"; ?>>
+                    <input type="hidden" value="delete" name="method" />
+                    <input type="hidden" value="<?php echo $comment[0]; ?>" name="id" />
+                    <button type="submit">コメントを消す</button>
+                </form>
+            </div>
+            <?php endforeach; ?>
+        </div>
+    </main>
 </body>
 
 </html>
